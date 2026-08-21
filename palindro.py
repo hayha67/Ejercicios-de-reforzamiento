e5f1fc160f7a1690dc092d07145e0ba21eb3ca83
hayha67 <fvalenciano278@gmail.com>
2026-08-20 20:01:51 -0600

    recientes

---
+++ palindro.py
@@ -0,0 +1,17 @@
+def es_palindromo(texto):
+    texto = texto.lower()
+    limpio = ""
+    for caracter in texto:
+        if caracter != " ":
+            limpio += caracter
+    return limpio == limpio[::-1], limpio
+
+if __name__ == "__main__":
+    entrada = input("Ingrese una frase: ")
+    resultado, cadena_limpia = es_palindromo(entrada)
+    
+    if resultado:
+        print("Es un palíndromo")
+    else:
+        print("No es palíndromo")
+    print("Longitud de la cadena limpia:", len(cadena_limpia))
